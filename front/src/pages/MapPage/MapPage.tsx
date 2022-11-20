c24768c28b236cb2f4ca0405783a811c80d4a7e8
Reedus0 <lazsasha2006@gmail.com>
2022-11-20 14:21:09 +0300

    added map

--- front/src/pages/MapPage/MapPage.tsx
+++ front/src/pages/MapPage/MapPage.tsx
@@ -1,21 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Map, Marker } from "pigeon-maps"
+import { API } from '../../api/API'
 
 
 const MapPage = () => {
 
     const height = window.innerHeight - 111
-    
+    const api = new API()
 
-    return (
-        <div className='map'>
+    const getCars = async () => {
+        const response: Response = await api.postRequest("/api/geo/get")
+        const responseJSON = await response.clone().json()
+        markersSet([...markers, ...responseJSON])
+    }
 
-            <Map height={height} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
-                <Marker width={50} anchor={[50.879, 4.6997]} />
-            </Map>
-        </div>
-    )
+    const getLocation = async () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(addPosition);
+        }
+    }
+
+    const addPosition = async (position: any) => {
+        const response: Response = await api.postRequest("/api/geo/add", {
+            car_id: 1,
+            lat: position['coords']['latitude'],
+            lon: position['coords']['longitude']
+        })
+
+        markersSet([ ...markers, {
+            "latitude": position['coords']['latitude'],
+            "longitude": position['coords']['longitude']
+        }])
+
+}
+
+const addGeo = async () => {
+    getLocation()
+}
+
+useEffect(() => {
+    addGeo()
+}, [])
+
+const [markers, markersSet] = useState<{"latitude": number, "longitude": number}[]>([])
+
+return (
+    <div className='map'>
+
+        <Map height={height} defaultCenter={[45.0200, 38.5900]} defaultZoom={11}>
+            {markers.map(marker => <Marker width={50} anchor={[marker['latitude'], marker['longitude']]} />)}
+
+        </Map>
+    </div>
+)
 }
 
 export default MapPage
